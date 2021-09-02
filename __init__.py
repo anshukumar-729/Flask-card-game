2dbd548adda39cb505ae6c82399759d96bc30739
Anshu kumar <anshukumar93.40@gmail.com>
2021-09-02 11:44:10 +0530

    initial commit

---
+++ __init__.py
@@ -0,0 +1,440 @@
+import json
+import random
+from flask import Flask, render_template, request, jsonify
+from flask.wrappers import Response
+
+app = Flask(__name__)
+
+@app.route("/home")
+def home():
+    return render_template("Home.html")   
+@app.route("/zone" )
+def zone():
+    return render_template("InputOutput.html")  
+@app.route("/")
+def start():
+    return render_template("start.html")  
+
+@app.route("/call", methods=["POST"])
+
+def processJSON2(): 
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    value=(jsonObj["mycall"])
+    cards=[114,113,112,111,110,109,108,107,106,105,104,103,102,214,213,212,211,210,209,208,207,206,205,204,203,202,314,313,312,311,310,309,308,307,306,305,304,303,302,414,413,412,411,410,409,408,407,406,405,404,403,402]
+    
+    if(value=="n"):
+        random.shuffle(cards)
+        k=0
+        for i in range(4):
+            for j in range(13):
+                player[i][j]=cards[k]
+                k+=1
+        l1=[]
+        l2=[]
+        l3=[]
+        l4=[]
+        for i in range(4):
+            for j in range(13):
+                if((player[i][j]//100)==1):
+                    l1.append(player[i][j])
+                elif((player[i][j]//100)==2):
+                    l2.append(player[i][j])
+                elif((player[i][j]//100)==3):
+                    l3.append(player[i][j])
+                else:
+                    l4.append(player[i][j])
+            p[i][0]=l1
+            p[i][1]=l2
+            p[i][2]=l3
+            p[i][3]=l4
+            l1=[]
+            l2=[]
+            l3=[]
+            l4=[]
+        for i in range (4):
+            for j in range(4):
+                p[i][j].sort()
+        print("player1: "+str(p[0]))
+        print()
+        print("player2: "+str(p[1]))
+        print()
+        print("player3: "+str(p[2]))
+        print()
+        print("player4: "+str(p[3]))
+        print()
+        up=random.choice(p)
+        ind=p.index(up)
+        print("You are playing with these cards "+str(up))
+        print("You are player "+str(ind+1))
+        print()
+        print("Let's Start ...")
+        count=0
+        start=1
+        pn=1
+        i=0
+        round=0
+        crd5=0
+        win=0
+        ind2=0
+        cds=""
+        dic["count"]=0
+        dic["start"]=1
+        dic["pn"]=1
+        dic["i"]=0
+        dic["round"]=0
+        dic["win"]=0
+        dic["ind2"]=0
+        dic["up"]=up
+        dic["ma"]=0
+        dic["ind"]=ind
+        dic["pl"]=0
+        dic["card"]="000"
+
+        ind=dic["ind"]
+        up=""
+        for m in p[ind]:
+                for n in m:
+                    up+=str(n)
+        c=[0,0,0]
+        temp3=-1
+        temp4=0
+        for i in p:
+            temp4+=1
+            if(temp4-1==ind):
+                continue
+            temp3+=1
+            for j in i:
+                for k in j:
+                    if k%100>10:
+                        c[temp3]+=1
+        dic["c"]=c
+        dic2=str(up)+"e"+str(ind)
+        for i in range(3):
+            if(c[i]<10):
+                c[i]="0"+str(c[i])
+            else:
+                c[i]=str(c[i]) 
+            dic2+=str(c[i])  
+        response=str(dic2)     
+        return response
+    elif(value=="t"):
+        cards=open("./static/cards.txt","r")
+        content=cards.read()
+        count2=0
+        indn=""
+        ln1=""
+        ln2=""
+        ln3=""
+        ln4=""
+        d1=0
+        d2=0
+        d3=0
+        d4=0
+        d5=0
+        for con in content:
+            if(con=="="):
+                count2+=1
+                continue
+            if count2==1 and d1==0:
+                if(con=="."):
+                    d1=1
+                else:
+                    print((con))
+                    indn+=(con)
+            if count2==2 and d2==0:
+                if(con=="."):
+                    d2=1
+                else:
+                    ln1+=con
+            if count2==3 and d3==0:
+                if(con=="."):
+                    d3=1
+                else:
+                    ln2+=con
+            if count2==4 and d4==0:
+                if(con=="."):
+                    d4=1
+                else:
+                    ln3+=con
+            if count2==5 and d5==0:
+                if(con=="."):
+                    d5=1
+                else:
+                    ln4+=con
+        print(eval(ln1))
+        print(eval(ln2))
+        print(eval(ln3))
+        print(eval(ln4))
+        indn=eval(indn)
+        print(indn)
+        dic["ind"]=indn
+        p[0]=eval(ln1)
+        p[1]=eval(ln2)
+        p[2]=eval(ln3)
+        p[3]=eval(ln4)
+        up=""
+        for m in p[(indn)]:
+                for n in m:
+                    up+=str(n)
+        c=[0,0,0]
+        temp3=-1
+        temp4=0
+        for i in p:
+            temp4+=1
+            if(temp4-1==indn):
+                continue
+            temp3+=1
+            for j in i:
+                for k in j:
+                    if k%100>10:
+                        c[temp3]+=1
+        dic["c"]=c
+        dic2=str(up)+"e"+str(indn)
+        for i in range(3):
+            if(c[i]<10):
+                c[i]="0"+str(c[i])
+            else:
+                c[i]=str(c[i]) 
+            dic2+=str(c[i])  
+        response=str(dic2)     
+        return response
+    else:
+        if(int(value)>9):
+            dic["cp"]=str(value)
+        else:
+            dic["cp"]=str(0)+str(value)
+            print(dic)
+        return "start"
+                
+
+
+            
+
+@app.route("/cards", methods=["POST"])
+
+def processJSON():
+    jsonStr = request.get_json()
+    jsonObj = json.loads(jsonStr)
+    value=(jsonObj['value'])
+    print(dic)
+    if(value=="yes"):
+        return "yes"
+    elif(value=="no"):
+        return "no"
+    else:
+        round=dic["round"]
+        pn=dic["pn"]
+        i=dic["i"]
+        start=dic["start"]
+        count=dic["count"]
+        win=dic["win"]
+        ind2=dic["ind2"]
+        ma=dic["ma"]
+        ind=dic["ind"]
+        up=""
+        pl=dic["pl"]
+        card=dic["card"]
+        cp=dic["cp"]
+        c=dic["c"]
+        win2=0
+        ma2=0
+        if(value!="over"):
+        
+            value=int(value)
+            
+            print(ind2)
+            if(value!=0):    
+                if pn==i+1:
+                    count+=1
+                    dic["count"]=count
+                    pn+=1
+                    dic["pn"]=pn
+                    temp=list(filter(None,p[i]))
+                    print("st"+str(start))
+                    if(start==1):
+                        start=0
+                        if(ind==i):
+                            crd1=value
+                            ma=crd1
+                            win=pn
+                            ind2=crd1//100
+                            dic["ind2"]=ind2
+                            p[i][ind2-1].remove(crd1)
+                            card=crd1
+                            pl=pn-1
+                        else:
+                            crd2=sorted(temp,key=lambda x: (x[-1]%100))[-1][-1]
+                            ind2=crd2//100
+                            dic["ind2"]=ind2
+                            p[i][ind2-1].remove(crd2)
+                            card=crd2
+                            pl=pn-1
+                            ma=crd2
+                            win=pn
+                    else:
+                        if(ind==i):
+                            crd3=value
+                            if (crd3//100==ind2):
+                                if crd3>=ma:
+                                    ma=crd3
+                                    win=pn
+                                p[i][ind2-1].remove(crd3)
+                                card=crd3
+                                pl=pn-1
+                            else:
+                                ind6=crd3//100
+                                p[i][ind6-1].remove(crd3)
+                                card=crd3
+                                pl=pn-1
+                        else:
+                            if p[i][ind2-1]==[]:
+                                crd4=sorted(temp,key=lambda x: (x[0]%100))[0][0]
+                                ind3=crd4//100
+                                p[i][ind3-1].remove(crd4)
+                                card=crd4
+                                pl=pn-1
+                            else:
+                                find=False
+                                print(ind2)
+                                for k in (p[i][ind2-1]):
+                                    if k>=ma:
+                                        ma=k
+                                        win=pn
+                                        crd5=k
+                                        find=True
+                                        p[i][ind2-1].remove(crd5)
+                                        card=crd5
+                                        pl=pn-1
+                                        break
+                                if find==False:
+                                    crd5=p[i][ind2-1][0]
+                                    p[i][ind2-1].remove(crd5)
+                                    card=crd5
+                                    pl=pn-1
+                
+                # print("player1: "+str(p[0]))
+                # print()
+                # print("player2: "+str(p[1]))
+                # print()
+                # print("player3: "+str(p[2]))
+                # print()
+                # print("player4: "+str(p[3]))
+                print(p[ind])
+                
+                i+=1
+                dic["count"]=count
+                print("cot"+str(count))
+                
+                if(i==4):
+                    i=0
+                    dic["i"]=i
+                if(pn==5):
+                    pn=1
+                    dic["pn"]=pn
+            
+
+                if(count==4):
+                    print("This round wins by player "+str(win-1))
+                    print("maximum card is: "+str(ma))
+                    win2=win-1
+                    ma2=ma
+                    count=0
+                    dic["count"]=count
+                    pn=win-1
+                    i=win-2
+                    dic["pn"]=pn
+                    start=1
+                    round+=1
+                    dic["round"]=round
+                else:
+                    win2=0
+                    ma2=0
+                dic["start"]=start
+                dic["i"]=i
+                dic["ma"]=ma
+                dic["win"]=win
+            print(p[ind])
+            for m in p[ind]:
+                    for n in m:
+                        up+=str(n)
+            print(up)
+            print(c)
+        dic2=str(up)+"e"+str(count)+str(start)+str(i)+str(round)+str(win2)+str(ma2)+str(ind2)+str(pn-1)+str(ind+1)+str(card)+str(pl)+str(ind)+(cp)
+        for i in range(3):
+            
+            dic2+=(c[i]) 
+        response=str(dic2)
+        return response
+if __name__ == "__main__":
+    l=[]
+    import random
+    cards=[114,113,112,111,110,109,108,107,106,105,104,103,102,214,213,212,211,210,209,208,207,206,205,204,203,202,314,313,312,311,310,309,308,307,306,305,304,303,302,414,413,412,411,410,409,408,407,406,405,404,403,402]
+# like if a no is 110 then the 1st dugut denote the suit and the last two digits denote the cardno
+# where 11 stands for joker 12 for queen 13 for king and 14 for ace
+    random.shuffle(cards)
+    global p
+    p=[[[]]*4 for i in range(4)]
+    global player
+    player=[[0]*13 for i in range(4)]
+
+    k=0
+    for i in range(4):
+        for j in range(13):
+            player[i][j]=cards[k]
+            k+=1
+    l1=[]
+    l2=[]
+    l3=[]
+    l4=[]
+    for i in range(4):
+        for j in range(13):
+            if((player[i][j]//100)==1):
+                l1.append(player[i][j])
+            elif((player[i][j]//100)==2):
+                l2.append(player[i][j])
+            elif((player[i][j]//100)==3):
+                l3.append(player[i][j])
+            else:
+                l4.append(player[i][j])
+        p[i][0]=l1
+        p[i][1]=l2
+        p[i][2]=l3
+        p[i][3]=l4
+        l1=[]
+        l2=[]
+        l3=[]
+        l4=[]
+    for i in range (4):
+        for j in range(4):
+            p[i][j].sort()
+    print("player1: "+str(p[0]))
+    print()
+    print("player2: "+str(p[1]))
+    print()
+    print("player3: "+str(p[2]))
+    print()
+    print("player4: "+str(p[3]))
+    print()
+    up=random.choice(p)
+    ind=p.index(up)
+    print("You are playing with these cards "+str(up))
+    print("You are player "+str(ind+1))
+    print()
+    print("Let's Start ...")
+    count=0
+    start=1
+    pn=1
+    i=0
+    round=0
+    crd5=0
+    win=0
+    ind2=0
+    cds=""
+    
+
+    global dic 
+    dic={"count":0,"start":1,"pn":1,"i":0,"round":0,"win":0,"ind2":0,"up":up,"ma":0,"ind":ind,"pl":0,"card":"000","c":0}
+
+    app.run(debug=True)
+    
---
+++ static/utility.js
@@ -0,0 +1,531 @@
+var next=""
+let obj1=[]
+let res1=["","","",""]
+let res2=[0,0,0,0]
+console.log(res2)
+let out=""
+var rnd=0
+var y1=0
+var y2=0
+var y3=0
+var y4=0
+var gc=[]
+let ind=0
+function call(targetURL) {
+	//Marshelling into JSON
+	
+	var dataToBeSent = "{";
+	if(typeof(document.forms[0]) == 'undefined' || document.forms[0] == null){
+		alert("HTML form element is not defined.");
+		return;
+	}
+	var x = document.forms[0].elements;
+	for (var i = 0; i < x.length; i++) {			
+		if((x[i].getAttribute("type") != null) && 
+			(x[i].getAttribute("type").toLowerCase() == 'submit' || x[i].getAttribute("type").toLowerCase() == 'button')) continue;
+		
+		if(i > 0) dataToBeSent += ",";
+		dataToBeSent += "\""+ x[i].getAttribute("name") +"\":\""+ x[i].value +"\"";
+		cp=parseInt(x[i].value);
+		console.log(cp)
+	}
+	dataToBeSent += "}";
+	dataToBeSent = JSON.stringify(dataToBeSent);
+	//Sending HTTP request to server
+	sendRequest2(targetURL, dataToBeSent);
+	return false;
+}
+function start(targetURL,ch) {
+	//Marshelling into JSON
+	if(ch=="g"){
+		localStorage.clear();
+		localStorage.setItem("res10","")
+		localStorage.setItem("res11","")
+		localStorage.setItem("res12","")
+		localStorage.setItem("res13","")
+		localStorage.setItem("res20",0)
+		localStorage.setItem("res21",0)
+		localStorage.setItem("res22",0)
+		localStorage.setItem("res23",0)
+		window.location.href = "./home";
+		
+	}
+	
+	var dataToBeSent = "{";
+	dataToBeSent += "\""+ "mycall"+"\":\""+ch+"\"";
+	dataToBeSent += "}";
+	dataToBeSent = JSON.stringify(dataToBeSent);
+	document.getElementById("str").style.display="none";
+	document.getElementById("stt").style.display="none";
+
+	document.getElementById("row").style.display="block";
+	//Sending HTTP request to server
+	sendRequest2(targetURL, dataToBeSent);
+	return false;
+}
+function sendRequest2(targetURL, jsonData){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			showOutput2(this.responseText);
+		} else if (this.readyState == 4){
+			alert("Error occurred with status code: "+this.status);
+		}
+	};
+	xhttp.open("POST", targetURL, true);
+	xhttp.setRequestHeader("Content-type", "application/json");
+	xhttp.send(jsonData);
+}
+
+
+function showOutput2(output){
+	console.log(output)
+	let temp=""
+	let k=0
+	console.log(output)
+	ind=parseInt(output[output.length-7])
+	
+	indt=ind+1
+	out=""
+	out+="<h4>You are player no "+indt+"</h4><br>"
+	let ib=0;
+	gc.c1=parseInt(output[output.length-(6)]+output[output.length-(5)])
+	gc.c2=parseInt(output[output.length-(4)]+output[output.length-(3)])
+	gc.c3=parseInt(output[output.length-(2)]+output[output.length-(1)])
+	console.log(gc)
+	for (let i=0;i<4;i++){
+		if(i==ind){
+			continue
+		}
+		let it=i+1
+		out+="<h5>The call of player "+it+" is "+(output[output.length-(6-(ib*2))]+output[output.length-(5-(ib*2))])
+		ib+=1
+	}
+	if(output=="start"){
+		
+	window.location.href = "./zone";
+	}
+	for (let i=0;i<output.length;i++){
+		
+		if (output[i]=="e"){
+			break
+		}
+		temp+=output[i]
+		k+=1
+		if(k==3){
+			k=0
+			temp=parseInt(temp)
+			document.getElementById(temp).style.display="inline"
+			temp=""	
+		}
+	}
+	
+	var division = document.getElementById("output_div");
+	if(division === null || division == 'undefined'){			
+		division = document.createElement("div");	
+		division.setAttribute("id", "output_div");	
+		division.setAttribute("style", "text-align:center");
+		division.style.color = "blue";	
+		var element = document.getElementsByTagName("body");
+		element[0].appendChild(division);
+	}	
+	division.innerHTML = out
+}
+
+function cards(targetURL,no) {
+	//Marshelling into JSON
+	if(no=="r"){
+		document.getElementById("replay").style.display="none";
+		window.location.href = "./";
+	}
+	var dataToBeSent = "{";
+	var name="value";
+	var v=no;
+	console.log(no)
+	console.log(obj1)
+	if(v=="yes"){
+		console.log("yes")
+		dataToBeSent += "\""+ name+"\":\""+ v+"\"";
+	
+	}
+	else if(v=="no"){
+		console.log("no")
+		dataToBeSent += "\""+ name+"\":\""+ v+"\"";
+	
+	}
+	else{
+	if(next=="true"){
+		// if(v==1){
+		// 	return true}
+		
+			
+		if(obj1.includes(no)){
+			
+			document.getElementById(no).style.opacity="0.7";
+			document.getElementById(no).style.boxShadow="none";
+			document.getElementById(no).style.cursor="auto";
+			document.getElementById(no).style.height="130px";
+			document.getElementById(no).style.width="65px";
+			document.getElementById(no).style.transform="none";
+		}
+		else{
+			return true
+		}
+	document.getElementById("turn").style.visibility="hidden";
+	next="false"
+	dataToBeSent += "\""+ name+"\":\""+ v+"\"";
+	
+	
+}
+	
+	else if(v==0){
+		dataToBeSent+= "\""+ name+"\":\""+ 0+"\"";
+		
+		document.getElementById(0).style.display="none";
+		document.getElementById(1).style.display="block";
+	}
+	else if(v==1){
+		console.log("rnd")
+		console.log(rnd)
+		if(rnd==53){
+			dataToBeSent+= "\""+ name+"\":\""+ "over"+"\"";
+		}
+		else{
+		dataToBeSent+= "\""+ name+"\":\""+ 1+"\"";}
+	}
+	else{
+		return true
+	}
+}
+	dataToBeSent += "}";
+	dataToBeSent = JSON.stringify(dataToBeSent);
+	//Sending HTTP request to server
+	sendRequest(targetURL, dataToBeSent);
+	return false;
+}
+
+
+function sendRequest(targetURL, jsonData){
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+			showOutput(this.responseText);
+		} else if (this.readyState == 4){
+			alert("Error occurred with status code: "+this.status);
+		}
+	};
+	xhttp.open("POST", targetURL, true);
+	xhttp.setRequestHeader("Content-type", "application/json");
+	xhttp.send(jsonData);
+}
+
+function showOutput(output){
+	rnd+=1
+	console.log(rnd)
+	console.log(output)
+	if(output=="yes"){
+		rnd=0
+		out=""
+		next=""
+		window.location.href = "./home";
+	}
+	else if(output=="no"){
+		document.getElementById("continue").style.display="none";
+		document.getElementById("replay").style.display="block";
+		out="Total Scores are:"
+		for(let i=0;i<3;i++){
+			
+			out+="<h5>bot"+(i+1)+" = "+localStorage.getItem("res1"+i)+" = "+localStorage.getItem("res2"+i)+"</h5><br>"
+			
+			res2[i]=localStorage.getItem("res2"+i)
+
+		}
+		out+="<h5>Your = "+localStorage.getItem("res1"+3)+" = "+localStorage.getItem("res2"+3)+"</h5><br>"
+		res2[3]=localStorage.getItem("res2"+3)
+		m1=Math.max(res2[0],res2[1],res2[2],res2[3])
+		if(res2[0]==(m1)){
+			out+="<h4>Bot1 win the Series!!!</h4>"
+		}
+		else if(res2[1]==(m1)){
+			out+="<h4>Bot2 win the Series!!!</h4>"
+		}
+		else if(res2[2]==m1){
+			out+="<h4>Bot3 win the Series!!!</h4>"
+		}
+		else if(res2[3]==(m1)){
+			out+="<h4>You win the Series!!!</h4>"
+		}
+		localStorage.clear();
+		var division = document.getElementById("output_div");
+	if(division === null || division == 'undefined'){			
+		division = document.createElement("div");	
+		division.setAttribute("id", "output_div");	
+		division.setAttribute("style", "text-align:center");
+		division.style.color = "blue";	
+		var element = document.getElementsByTagName("body");
+		element[0].appendChild(division);
+	}	
+
+	division.innerHTML = out
+
+	}
+	
+	else {
+	const c1=parseInt(output[output.length-6]+output[output.length-5])
+	const c2=parseInt(output[output.length-4]+output[output.length-3])
+	const c3=parseInt(output[output.length-2]+output[output.length-1])
+	const cp=parseInt(output[output.length-8]+output[output.length-7])
+	const ind=parseInt(output[output.length-9])
+	const p=parseInt(output[output.length-10])
+	const i=parseInt(output[output.length-15])
+	const j=parseInt(output[output.length-14])
+	const card=parseInt(output[output.length-13]+output[output.length-12]+output[output.length-11])
+	
+	const win=parseInt(output[output.length-20])
+	const ma=parseInt(output[output.length-19]+output[output.length-18]+output[output.length-17])
+
+
+		
+	console.log(res1)
+	console.log(res2)
+
+	
+	if(parseInt(rnd)>53){
+		document.getElementById("continue").style.display="block";
+		document.getElementById("cards").style.opacity="0.3";
+		document.getElementById("turn").style.visibility="hidden";
+		out="<h2>Scores are:</h2><br>"
+		var w0=[0,0,0,0]
+		let c0=[c1,c2,c3,cp]
+		console.log(c0)
+		let y0=[y1,y2,y3,y4]
+		let j=-1
+		
+		console.log(res1)
+		for (let i=0;i<3;i++){
+			j+=1
+			if(j==ind){
+				i-=1
+				continue
+			}
+			if(c0[i]>y0[j]){
+				out+="<h5>bot"+(i+1)+" = - 10*"+c0[i]+"(wins = "+y0[j]+", calls = "+c0[i]+")</h5><br>"
+				res1[i]+=" - 10*"+c0[i]
+				w0[i]=-10*c0[i]
+				res2[i]+=-10*c0[i]
+			}
+			else{
+				out+="<h5>bot"+(i+1)+" = 10*"+c0[i]+"+("+y0[j]+"-"+c0[i]+") (wins = "+y0[j]+", calls = "+c0[i]+")</h5><br>"
+				res1[i]+="+10*"+c0[i]+"+("+y0[j]+"-"+c0[i]+")"
+				w0[i]=10*c0[i]+(y0[j]-c0[i])
+				res2[i]+=10*c0[i]+(y0[j]-c0[i])
+			}
+			
+			
+		}
+		if(c0[3]>y0[ind]){
+			out+="<h5>Your = - 10*"+c0[3]+"(wins = "+y0[ind]+", calls = "+c0[3]+")</h5><br>"
+			res1[3]+=" - 10*"+c0[3]
+			w0[3]=-10*c0[3]
+			res2[3]+=-10*c0[3]
+		}
+		else{
+			out+="<h5>Your = 10*"+c0[3]+"+("+y0[ind]+"-"+c0[3]+") (wins = "+y0[ind]+", calls = "+c0[3]+")</h5><br>"
+			res1[3]+="+10*"+c0[3]+"+("+y0[ind]+"-"+c0[3]+")"
+			w0[3]=10*c0[3]+(y0[ind]-c0[3])
+			res2[3]+=10*c0[3]+(y0[ind]-c0[3])
+		}
+		
+		for (let i=0; i<4;i++){
+		localStorage.setItem("res1"+i, localStorage.getItem("res1"+i)+res1[i]);
+		localStorage.setItem("res2"+i, (parseInt(localStorage.getItem("res2"+i))+parseInt(res2[i])));
+		}
+		console.log("local")
+		console.log(localStorage)
+		console.log(gc)
+		console.log(res1)
+		console.log(res2)
+		console.log(w0)
+		console.log(Math.max(w0[0],w0[1],w0[2],w0[3]))
+		if(w0[0]==Math.max(Math.max(w0[0],w0[1],w0[2],w0[3]))){
+			out+="<h4>Bot1 is the winner!!!</h4>"
+		}
+		else if(w0[1]==(Math.max(w0[0],w0[1],w0[2],w0[3]))){
+			out+="<h4>Bot2 is the winner!!!</h4>"
+		}
+		else if(w0[2]==Math.max(w0[0],w0[1],w0[2],w0[3])){
+			out+="<h4>Bot3 is the winner!!!</h4>"
+		}
+		else if(w0[3]==(Math.max(w0[0],w0[1],w0[2],w0[3]))){
+			out+="<h4>You are the winner!!!</h4>"
+		}
+		console.log(out)
+			
+	}
+	else{
+		
+		console.log(win)
+	console.log(ma)
+	switch(parseInt(card/100)){
+		case 1: suit="Spades"
+				break
+		case 2: suit="Clubs"
+				break
+		case 3: suit="Diamond"
+				break
+		case 4: suit="Hearts"
+				break
+		default: suit="undefined"
+	}
+	switch(card%100){
+		case 01: Cno="1"
+				break
+		case 02: Cno="2"
+				break
+		case 03: Cno="3"
+				break
+		case 04: Cno="4"
+				break
+		case 05: Cno="5"
+				break
+		case 06: Cno="6"
+				break
+		case 07: Cno="7"
+				break
+		case 08: Cno="8"
+				break
+		case 09: Cno="9"
+				break
+		case 10: Cno="10"
+				break
+		case 11: Cno="Joker"
+				break
+		case 12: Cno="Queen"
+				break
+		case 13: Cno="King"
+				break
+		case 14: Cno="Ace"
+				break
+		default: Cno="undefined"
+	}
+	if(p!="0"){
+	out+="<h5>"+Cno+" of "+suit+" chosen by player "+p+"</h5>"}
+	if(i+1==j && rnd!=53){
+		document.getElementById("turn").style.visibility="visible";
+		next="true"
+		document.getElementById(1).style.visibility="hidden";
+	}
+	else{
+		document.getElementById(1).style.visibility="visible";
+	}
+	if(win!=0 && ma%100!=00){
+		switch(parseInt(win)){
+			case 1:y1+=1
+				break
+			case 2:y2+=1
+				break
+			case 3:y3+=1
+				break
+			case 4:y4+=1
+				break
+		}
+		switch(parseInt(ma/100)){
+			case 1: suit="Spades"
+					break
+			case 2: suit="Clubs"
+					break
+			case 3: suit="Diamond"
+					break
+			case 4: suit="Hearts"
+					break
+			default: suit="undefined"
+		}
+		switch(ma%100){
+			case 01: Cno="1"
+					break
+			case 02: Cno="2"
+					break
+			case 03: Cno="3"
+					break
+			case 04: Cno="4"
+					break
+			case 05: Cno="5"
+					break
+			case 06: Cno="6"
+					break
+			case 07: Cno="7"
+					break
+			case 08: Cno="8"
+					break
+			case 09: Cno="9"
+					break
+			case 10: Cno="10"
+					break
+			case 11: Cno="Joker"
+					break
+			case 12: Cno="Queen"
+					break
+			case 13: Cno="King"
+					break
+			case 14: Cno="Ace"
+					break
+			default: Cno="undefined"
+		}
+		out+="<br><h4>This round wins by player "+win+"<br> And the highest value cards is "+Cno+" of "+suit+"</h4>"
+
+	}
+	let k=0
+	let temp=""
+	let temp2=0
+	let temp3=0
+	let temp4=0
+	obj1=[]
+	for (let i=0;i<output.length;i++){
+		
+		if (output[i]=="e"){
+			break
+		}
+		temp+=output[i]
+		k+=1
+		if(k==3){
+			k=0
+			temp=parseInt(temp)
+			obj1.push(temp);
+			document.getElementById(temp).style.display="inline"
+			temp=""	
+		
+		
+		
+		}
+		
+		
+	}
+	console.log(cp)
+	console.log(ind)
+	}
+	console.log("p"+p)
+	if(p!=0 || rnd==54){
+		var division = document.getElementById("output_div");
+	if(division === null || division == 'undefined'){			
+		division = document.createElement("div");	
+		division.setAttribute("id", "output_div");	
+		division.setAttribute("style", "text-align:center");
+		division.style.color = "blue";	
+		var element = document.getElementsByTagName("body");
+		element[0].appendChild(division);
+	}	
+
+	division.innerHTML = out
+	if(win!=0 && ma%100!=00){
+		out=""
+	}}
+}
+	
+	
+	
+	
+	// if(output["pn"]==output["ind"])	{
+	// 	console.log("hi "+output["pn"])
+	// }
+	
+}
